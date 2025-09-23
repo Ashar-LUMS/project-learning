de57c79bc082a7bfd0dcaf48696e46f0bd8d5241
Amna Raza Khan <amnarazakhan02@gmail.com>
2025-09-23 15:14:51 +0500

    updated forget password

--- src/features/auth/forget-password.tsx
+++ src/features/auth/forget-password.tsx
@@ -9,7 +9,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/";
+import { Label } from "@/components/ui/label";
 import { supabase } from "../../supabaseClient";
 
 export function ForgotPasswordForm({
